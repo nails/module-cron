2b53f81547baca9c1636b76b05a9afd5828e2f27
Pablo de la Pena <p@shedcollective.org>
2014-06-21 13:23:04 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Cron Module for Nails
+---
+This is the Cron module for nails, it provides a centralsied touchpoint for schedule task handling.
+
+
+http://nailsapp.co.uk/modules/cron
---
+++ cron/language/english/cron_lang.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* Name:			CRON Langfile
+*
+* Description:  Language file for CRON module
+* 
+*/
+
+	$lang['unauthorised']	= 'Unauthorised';
