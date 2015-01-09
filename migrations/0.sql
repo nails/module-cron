565fc7124b6792d1c70df87ccece96ffb4226b4b
Pablo de la Peña <p@shedcollective.org>
2015-01-09 14:16:00 +0000

    Migration Joy

---
+++ migrations/0.sql
@@ -0,0 +1,4 @@
+# INITIAL ADMIN DB
+# This is the schema of the Cron module database as of 09/01/2015
+DROP TABLE IF EXISTS `nails_log_cron`;
+CREATE TABLE `nails_log_cron` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT, `task` varchar(150) NOT NULL DEFAULT '', `duration` double NOT NULL, `message` varchar(500) DEFAULT NULL, `created` datetime NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
