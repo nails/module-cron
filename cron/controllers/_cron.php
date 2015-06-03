e258521ac60fda9146d0875d14353c5d6f7ef145
Pablo de la Peña <p@shedcollective.org>
2015-06-03 14:42:41 +0100

    PSR-2'd _cron.php and made cron logging less verbose
    Alsoa dded the notion of a "task ID" to allow for simultaneous tasks to log to the same file yet still be identifiable.

--- cron/controllers/_cron.php
+++ cron/controllers/_cron.php
@@ -12,96 +12,104 @@
 
 class NAILS_Cron_Controller extends NAILS_Controller
 {
-	protected $start;
-	protected $task;
+    protected $iStart;
+    protected $sTask;
+    protected $sTaskId;
 
+    // --------------------------------------------------------------------------
 
-	// --------------------------------------------------------------------------
+    /**
+     * Construct the controller
+     */
+    public function __construct()
+    {
+        parent::__construct();
 
+        // --------------------------------------------------------------------------
 
-	/**
-	 * Constructor
-	 *
-	 * @access	public
-	 * @param	none
-	 * @return	void
-	 **/
-	public function __construct()
-	{
-		parent::__construct();
+        //  Load language file
+        $this->lang->load('cron');
 
-		// --------------------------------------------------------------------------
+        // --------------------------------------------------------------------------
 
-		//	Load language file
-		$this->lang->load( 'cron' );
+        //  Command line only
+        if (strtoupper(ENVIRONMENT) == 'PRODUCTION' && ! $this->input->is_cli_request()) {
+            header($this->input->server('SERVER_PROTOCOL') . ' 401 Unauthorized');
+            die('<h1>' . lang('unauthorised') . '</h1>');
+        }
 
-		// --------------------------------------------------------------------------
+        // --------------------------------------------------------------------------
 
-		//	Command line only
-		if ( strtoupper( ENVIRONMENT ) == 'PRODUCTION' && ! $this->input->is_cli_request() ) :
+        //  E_ALL E_STRICT error reporting, for as error free code as possible
+        error_reporting(E_ALL|E_STRICT);
+    }
 
-			header( $this->input->server( 'SERVER_PROTOCOL' ) . ' 401 Unauthorized' );
-			die( '<h1>' . lang( 'unauthorised' ) . '</h1>' );
+    // --------------------------------------------------------------------------
 
-		endif;
+    /**
+     * Kicks off cron jobs and specifies logging details
+     * @param  string $sLogDir  The log [sub] directory
+     * @param  string $sLogFile The log file
+     * @param  string $sTask    The human description of the task
+     * @param  string $sTaskId  An optional identifier for the task (useful for when multiple jobs of the same type might run in parallel)
+     * @return void
+     */
+    protected function _start($sLogDir, $sLogFile, $sTask, $sTaskId)
+    {
+        //  Tick tock tick...
+        $this->iStart  = microtime(true) * 10000;
+        $this->sTask   = trim($sTask);
+        $this->sTaskId = trim($sTaskId);
 
-		// --------------------------------------------------------------------------
+        //  Set log details
+        _LOG_FILE('cron/' . $sLogDir . '/' . $sLogFile . '-' . date('Y-m-d') . '.php');
+        $this->writeLog('Starting job [' . $this->sTask . ']...');
+    }
 
-		//	E_ALL E_STRICT error reporting, for as error free code as possible
-		error_reporting( E_ALL|E_STRICT );
-	}
+    // --------------------------------------------------------------------------
 
+    /**
+     * Marks the end of a task
+     * @param  string $sLogMessage An optional message to add
+     * @return void
+     */
+    protected function _end($sLogMessage = null)
+    {
+        //  How'd we do?
+        $iEnd      = microtime(true) * 10000;
+        $iDuration = ($iEnd - $this->iStart) / 10000;
 
-	// --------------------------------------------------------------------------
+        // --------------------------------------------------------------------------
 
+        $this->writeLog('Finished job [' . $this->sTask . ']');
+        $this->writeLog('Job took ' . number_format($iDuration, 5) . ' seconds');
 
-	protected function _start( $log_dir, $log_file, $task )
-	{
-		//	Tick tock tick...
-		$this->start	= microtime( TRUE ) * 10000;
-		$this->task		= $task;
+        // --------------------------------------------------------------------------
 
-		// --------------------------------------------------------------------------
+        //  Write this to the DB log
+        $aData              = array();
+        $aData['task']      = $this->sTask;
+        $aData['duration']  = $iDuration;
+        $aData['message']   = $sLogMessage;
 
-		//	Set log details
-		_LOG_FILE( 'cron/' . $log_dir . '/' . $log_file . '-' . date( 'Y-m-d' ) . '.php' );
-		_LOG( 'Starting job [' . $this->task . ']...' );
-		_LOG();
-	}
+        $this->db->set($aData);
+        $this->db->set('created', 'NOW()', false);
+        $this->db->insert(NAILS_DB_PREFIX . 'log_cron');
+    }
 
+    // --------------------------------------------------------------------------
 
-	// --------------------------------------------------------------------------
+    /**
+     * Writes a line to the log file, prepends the task ID if there is one
+     * @param  string $sLine The line to write
+     * @return void
+     */
+    protected function writeLog($sLine)
+    {
+        if ($this->sTaskId) {
+            $sLine = '[' . $this->sTaskId. '] ' . $sLine;
+        }
 
-
-	protected function _end( $log_message = NULL )
-	{
-		//	How'd we do?
-		$_end		= microtime( TRUE ) * 10000;
-		$_duration	= ( $_end - $this->start ) / 10000;
-
-		// --------------------------------------------------------------------------
-
-		_LOG();
-		_LOG( 'Finished job [' . $this->task . ']' );
-		_LOG( 'Job took ' . number_format( $_duration, 5 ) . ' seconds' );
-		_LOG();
-		_LOG( '----------------------------------------' );
-		_LOG();
-
-		// --------------------------------------------------------------------------
-
-		//	Write this to the DB log
-		$_data				= array();
-		$_data['task']		= $this->task;
-		$_data['duration']	= $_duration;
-		$_data['message']	= $log_message;
-
-		$this->db->set( $_data );
-		$this->db->set( 'created', 'NOW()', FALSE );
-		$this->db->insert( NAILS_DB_PREFIX . 'log_cron' );
-	}
+        _LOG($sLine);
+    }
 }
-
-
-/* End of file _cron.php */
-/* Location: ./application/modules/cron/controllers/_cron.php */
